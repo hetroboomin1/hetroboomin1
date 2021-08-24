66d025c9fd89bdadace13b530fd1e51e8603acee
hettira <hchhlam@gmail.com>
2021-08-24 01:11:40 -0400

    rewrote the model for OHLC

--- src/main/java/com/Swaggu/MrktDataApi/Models/OHLC.java
+++ src/main/java/com/Swaggu/MrktDataApi/Models/OHLC.java
@@ -5,49 +5,61 @@ import java.util.List;
 
 public class OHLC {
     
-    private List<BigDecimal> ohlcData;
+    private BigDecimal time;
+    private BigDecimal eTime;
+    private BigDecimal open;
+    private BigDecimal high;
+    private BigDecimal low;
+    private BigDecimal close;
+    private BigDecimal VWAP;
+    private BigDecimal volume;
+    private int count;
 
     public OHLC(List<BigDecimal> ohlcData) {
-        this.ohlcData = ohlcData;
-    }
-
-    public void setOHLCData(List<BigDecimal> ohlcData) {
-        this.ohlcData = ohlcData;
+        this.time = ohlcData.get(0);
+        this.eTime = ohlcData.get(1);
+        this.open = ohlcData.get(2);
+        this.high = ohlcData.get(3);
+        this.low = ohlcData.get(4);
+        this.close = ohlcData.get(5);
+        this.VWAP = ohlcData.get(6);
+        this.volume = ohlcData.get(7);
+        this.count = ohlcData.get(8).intValue();
     }
 
     public BigDecimal getTime() {
-        return ohlcData.get(0);
+        return time;
     }
 
     public BigDecimal getETime() {
-        return ohlcData.get(1);
+        return eTime;
     }
 
     public BigDecimal getOpen() {
-        return ohlcData.get(2);
+        return open;
     }
 
     public BigDecimal getHigh() {
-        return ohlcData.get(3);
+        return high;
     }
 
     public BigDecimal getLow() {
-        return ohlcData.get(4);
+        return low;
     }
 
     public BigDecimal getClose() {
-        return ohlcData.get(5);
+        return close;
     }
 
     public BigDecimal getVWAP() {
-        return ohlcData.get(6);
+        return VWAP;
     }
 
     public BigDecimal getVolume() {
-        return ohlcData.get(7);
+        return volume;
     }
 
-    public BigDecimal getCount() {
-        return ohlcData.get(8);
+    public int getCount() {
+        return count;
     }
 }
