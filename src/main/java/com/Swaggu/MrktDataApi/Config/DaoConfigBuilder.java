8c78490c893e18059526cdafd1c164a20323eceb
hettira <hchhlam@gmail.com>
2021-09-17 00:04:17 -0400

    implemented mrktdao instead of specific pair dao

--- src/main/java/com/Swaggu/MrktDataApi/Config/DaoConfigBuilder.java
+++ src/main/java/com/Swaggu/MrktDataApi/Config/DaoConfigBuilder.java
@@ -3,39 +3,51 @@ package com.Swaggu.MrktDataApi.Config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 // @Configuration
 @Component("databaseConfig")
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "database")
-public class DatabaseConfig {
+public class DaoConfigBuilder {
+    private final Logger logger = LoggerFactory.getLogger(DaoConfigBuilder.class);
+
     private String driver;
     private String username;
     private String password;
-    private String databaseUrl;
+    private String url;
 
-    public void setDatabaseUrl(String databaseUrl){
-        this.databaseUrl = databaseUrl;
+    public void setUrl(String url) {
+        logger.info("Database url set to " + url);
+        this.url = url;
     }
-    public String getDatabaseUrl(){
-        return databaseUrl;
+
+    public String getUrl() {
+        return url;
     }
-    public void setUsername(String username){
+
+    public void setUsername(String username) {
         this.username = username;
     }
-    public String getUsername(){
+
+    public String getUsername() {
         return username;
     }
-    public void setPassword(String password){
+
+    public void setPassword(String password) {
         this.password = password;
     }
-    public String getPassword(){
+
+    public String getPassword() {
         return password;
     }
-    public void setDriver(String driver){
+
+    public void setDriver(String driver) {
         this.driver = driver;
     }
-    public String getDriver(){
+
+    public String getDriver() {
         return driver;
     }
 }
---
+++ src/main/java/com/Swaggu/MrktDataApi/DatabaseApi/KrakenDatabase/Factory/DaoFactoryImp.java
@@ -0,0 +1,43 @@
+package com.Swaggu.MrktDataApi.DatabaseApi.KrakenDatabase.Factory;
+// package com.Swaggu.MrktDataApi.DatabaseApi;
+
+// import java.util.HashMap;
+// import java.util.Map;
+
+// import com.Swaggu.MrktDataApi.Config.DaoFactoryConfig;
+
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.stereotype.Component;
+// import org.springframework.web.server.ResponseStatusException;
+
+// @Component
+// public class DaoFactoryImp implements DaoFactory {
+//     private Logger logger = LoggerFactory.getLogger(DaoFactoryImp.class);
+
+//     private Map<String, MrktDao> listDaos = new HashMap<>();
+
+//     //Singleton factory?
+//     @Autowired
+//     public DaoFactoryImp(DaoFactoryConfig daoFactoryConfig){
+//         // this.listDaos = daoFactoryConfig.getDaos();
+//     }
+
+//     @Override
+//     public MrktDao getMrktDao(String ticker) {
+//         if(listDaos.get(ticker) == null){
+//             logger.info("ticker not found");
+//             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ticker does not exist");
+//         }
+//         return listDaos.get(ticker);
+//     }
+
+//     @Override
+//     public MrktDao setMrktDao(String ticker) {
+//         // TODO Auto-generated method stub
+//         return null;
+//     }
+// }
